03203361d7f23bbee4c970151954d602572c607a
Aleksandr Buganov <rivares89@gmail.com>
2023-10-08 02:27:31 +0300

    New

--- src/channel.py
+++ src/channel.py
@@ -1,11 +1,19 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+# import isodate
+
+api_key: str = os.getenv('YOUTUBE_API_KEY')
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
-class Channel:
-    """Класс для ютуб-канала"""
-
+class
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
